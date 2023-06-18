89a8a7e76d22a2ed219e2ebdafa6676ac21c52d9
sagar <sagar@gmail.com>
2023-06-18 21:19:50 +0530

    commit9

--- pornima.groovy
+++ pornima.groovy
@@ -8,7 +8,7 @@ pipeline {
         }
         stage('Test') { 
             steps {
-                 echo 'test stage'
+                 sh 'mvn clean package'
             }
         }
         stage('Deploy') { 

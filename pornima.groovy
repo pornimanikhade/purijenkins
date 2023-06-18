54d31541a55448fbacade77ac6b2c8f6a35fe521
sagar <sagar@gmail.com>
2023-06-18 21:05:15 +0530

    commit4

--- pornima.groovy
+++ pornima.groovy
@@ -8,7 +8,26 @@ pipeline {
         }
         stage('build') { 
             steps {
-               sh 'mvn clean package'
+               sh 'mvn clean package'pipeline {
+    agent any 
+    stages {
+        stage('pull') { 
+            steps {
+                git branch: 'main', url: 'https://github.com/sagarfast/student-ui.git'
+            }
+        }
+        stage('builds') { 
+            steps {
+                sh 'mvn clean package'
+            }
+        }
+        stage('Deploy') { 
+            steps {
+                deploy adapters: [tomcat8(credentialsId: 'tomcat-creds-1', path: '', url: 'http://3.143.23.110:8080/')], contextPath: '/', war: '**/*.war'
+            }
+        }
+    }
+}
             }
         }
         stage('Deploy') { 

16a454f1ad5c3e6865b29728dfe78a4611d39516
sagar <sagar@gmail.com>
2023-06-18 22:03:20 +0530

    commit10

--- pornima.groovy
+++ pornima.groovy
@@ -13,7 +13,7 @@ pipeline {
         }
         stage('Deploy') { 
             steps {
-                 echo 'deploy stage'
+                deploy adapters: [tomcat8(credentialsId: 'tomcat-creds-1', path: '', url: 'http://3.141.192.77:8080/')], contextPath: '/', war: '**/*.war'
             }
         }
     }
